4f730a835b513399a6f484cc9219f22085e4c0c2
Ardita Burhop <kalendarperaden@gmail.com>
2020-11-13 13:57:50 -0600

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+let obj = {}
+let maxVal = 0
+let maxKey = ''
+
+for (let char of str){
+  //obj[char] = (obj[char]|| 0)+1
+  obj[char] = obj[char]+1 || 1
+}
+//console.log("obj THIS IS THE OBJ", obj)
+for (let key in obj) {
+if(maxVal < obj[key])
+maxKey = key   //if we flip these two statements tests fail
+maxVal = obj[key] //seems that maxKey is reassiged hence this order 
+}
+
+return maxKey
+}
 
 module.exports = maxChar;
