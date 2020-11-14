dd55695926ffe2c7e4eaeb65ebfc88eee23f4387
Ardita Burhop <kalendarperaden@gmail.com>
2020-11-13 18:05:12 -0600

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,27 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let arrChunk = []
+  for(let i=0; i<array.length; ++i){
+   let last = arrChunk[arrChunk.length - 1] //take the last elem in the chunk arr
+    if(!last || last.length === size) arrChunk.push([array[i]])
+    else last.push(array[i])
+  }
+ return arrChunk
+}
+//WITH SLICE
+
+// function chunk(array, size) {  //with slice
+//   let chunkedArr = []
+//   let index = 0
+//   while(index<array.length){
+//     let chunk = array.slice(index, index+size)
+//     chunkedArr.push(chunk)
+//     index+=size
+//   }
+
+//  return chunkedArr
+// }
 
 module.exports = chunk;
