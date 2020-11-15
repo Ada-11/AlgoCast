77658027aec80e4da1afb1440fff25562a6266f7
Ardita Burhop <kalendarperaden@gmail.com>
2020-11-14 18:51:50 -0600

    anagrams2

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,27 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//  let arrStr = str.split(' ')
+//  let nuArrStr = []
+//    for (let char of arrStr){
+//    let nuchar = char[0].toUpperCase() + (char.slice(1))
+//    nuArrStr.push(nuchar)
+//    }
+//    return nuArrStr.join(' ')
+//   }
+
+  function capitalize(str) {
+     let result = str[0].toUpperCase()
+     for (let i = 1; i<str.length; ++i){
+      if (str[i-1] === " ") {
+        result += str[i].toUpperCase()
+      } else {
+        result+=str[i]
+      }
+     }
+     return result
+    }
+
 
 module.exports = capitalize;
