a5fd2543a7c86b922e2f0e8ed37ef496ca218087
Ardita Burhop <kalendarperaden@gmail.com>
2020-11-16 18:12:51 -0600

    vowelcount1&2

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,29 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//  let counter = 0
+//  let vowel= "aeiou"
+//   for (let i=0;i<vowel.length; ++i){
+//     if (str.toLowerCase().includes(vowel[i])) {
+//       counter++
+//     } else {
+//       continue
+//     }
+//   }
+//   return counter
+// }
+
+//with RegEx
+ //match() returns the num of matches in an arr, or null if nothing is found
+ //regex g for global and i for case insensitive
+ //if matches is true return length if falsy return 0
+
+function vowels(str){
+
+let matches = str.match(/["aeiou"]/gi)
+return matches ? matches.length : 0
+
+}
 
 module.exports = vowels;
