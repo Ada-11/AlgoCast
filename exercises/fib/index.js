4f4e82d1db9d1bce0b05f4ad7b50d9f596c7e803
Ardita Burhop <kalendarperaden@gmail.com>
2020-11-18 13:34:25 -0600

    fib2solutions

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -7,7 +7,24 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+  //itirative
 
-function fib(n) {}
+// function fib(n) {
+//    let result = [ 0, 1]
+//   for( let i =2; i<=n; ++i){
+//     const a = result[i-2]
+//     const b = result[i-1]
+//     result.push(a+b)
+//   }
+//   return result[n]
+// }
+
+//RECURSIVE SOLUTION
+
+function fib(n) {
+ 
+  if ( n < 2) return n
+    return fib(n - 2) + fib( n - 1 )
+  }
 
 module.exports = fib;
