77b1a942cb55dfa852cde3c4be2704c96463a97e
Ardita Burhop <kalendarperaden@gmail.com>
2020-11-19 14:53:38 -0600

    eventsLibrary

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,34 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor(){
+    this.stack = {} //key with array values for each event
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if(this.stack[eventName]){
+      this.stack[eventName].push(callback)
+    }else{
+      this.stack[eventName]=[callback] //initiated with an array and callback inside
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if ( this.stack[eventName]){
+      for ( let cback of this.stack[eventName]) cback()
+    }
+
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    if(this.stack[eventName]) {
+      delete this.stack[eventName]
+  }
+}
 }
 
 module.exports = Events;
