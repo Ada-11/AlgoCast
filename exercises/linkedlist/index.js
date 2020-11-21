7d7249a35a77138d75b44300d55d9d47f4d99698
Ardita Burhop <kalendarperaden@gmail.com>
2020-11-20 22:53:40 -0600

    LLinsertFirst

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,22 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next =null){
+    this.data = data
+    this.next= next
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor(){
+    this.head = null
+  }
+insertFirst(data){
+  let nuNode = new Node(data, this.head)
+  this.head = nuNode
+  // this.head = new Node(data, this.head) in one line
+}
 
+}
 module.exports = { Node, LinkedList };
