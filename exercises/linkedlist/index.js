76ffe16bb3d01736042217ae6a82e7d20ae9ddc6
Ardita Burhop <kalendarperaden@gmail.com>
2020-11-21 19:33:26 -0600

    1stpartofLL

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -17,7 +17,77 @@ insertFirst(data){
   let nuNode = new Node(data, this.head)
   this.head = nuNode
   // this.head = new Node(data, this.head) in one line
+  //the node is added from the beg of the LL becoming the head that's why
+   //next is this.head
 }
+size(){
+  let counter = 0
+while (this.head){
+this.head = this.head.next
+counter ++
+}
+return counter
+}
+
+getFirst(){
+  return this.head
+}
+getLast(){
+  if (!this.head) return null
+
+  let node = this.head
+  while (node){
+    if (!node.next) return node
+
+    node= node.next
+  }
+}
+clear(){
+  let node = this.head
+while (node){
+   node=node.next //traversing
+   node.next=null  //making the refence null
+                    //do we have to delete the node or just not reference it?
+  //this.head= null  author's solution doesn't delete nodes in LL
+}
+}
+removeFirst(){
+  if (!this.head) {
+    return
+  }
+  this.head=this.head.next //one test fails for no reason here
+
+}
+removeLast(){
+  if (!this.head) return
+
+  if (!this.head.next){
+    this.head= null
+    return
+  }
+
+  let previous = this.head
+  let node = this.head.next
+  while (node.next){
+    previous=node
+    node=node.next
+
+  }
+  previous.next = null
+}
+insertLast(data){
+
+  let lastNode = this.getLast()
+  if(lastNode) {
+     lastNode.next = new Node(data)
+  }else{
+    this.head = new Node(data)
+
+  }
+
+}
+
+
 
 }
 module.exports = { Node, LinkedList };
