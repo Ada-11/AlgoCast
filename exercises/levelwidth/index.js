bbf737ec980d549b4631d6c31aeea7b4b2bd2b21
Ardita Burhop <kalendarperaden@gmail.com>
2020-12-08 22:09:24 -0600

    widthlevel

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,28 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+ const queue = [root, 's'];
+ const counter = [0];
+
+ while (queue.length >1){
+  const node = queue.shift();  //steps from BFTraversal
+
+  if (node === 's'){
+    counter.push(0);
+    queue.push('s');
+  } else {
+    queue.push(...node.children);
+    counter[counter.length -1]++;
+
+  }
+
+ }
+ return counter;
+}
+
+
+//whenever there is a refer to the root node, there's no tree, we're working with node class ( data & children)
+//2- width word == BF traversal
 
 module.exports = levelWidth;
