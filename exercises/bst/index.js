c0e982287312fde0aae7523b6f04c88c38391d85
Ardita Burhop <kalendarperaden@gmail.com>
2020-12-09 22:57:45 -0600

    BSTcontainsValueRecursive

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -31,9 +31,19 @@ class Node {
 
      } else if (data > this.data){
        this.right = new Node(data);
-
      }
+     }
+     contains(data){
+      if (data === this.data) return this;
 
+      if (data < this.data && this.left){
+
+          return this.left.contains(data)
+      } else if (data > this.data && this.right){
+
+          return this.right.contains(data);
+      }
+       return null;
      }
 
     }
