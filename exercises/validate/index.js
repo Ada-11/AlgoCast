e41dd40247c4e5cf51b6c7de679209fdc3363f4a
Ardita Burhop <kalendarperaden@gmail.com>
2020-12-15 22:39:40 -0600

    validate

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -7,7 +7,7 @@
 
 function validate(node, min = null, max = null) {
 
-  if(max !== null && node.data > max) {
+  if(max !== null && node.data > max) { //
     return false;
   }
   if(min!== null && node.data < min){
