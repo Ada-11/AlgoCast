969ed152da741837d01db8e371c6826205af9386
Ardita Burhop <kalendarperaden@gmail.com>
2020-12-28 22:30:59 -0600

    capitalizeReview

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -18,16 +18,29 @@
 //   }
 
   function capitalize(str) {
-     let result = str[0].toUpperCase()
-     for (let i = 1; i<str.length; ++i){
-      if (str[i-1] === " ") {
-        result += str[i].toUpperCase()
+    let result = str[0].toUpperCase()
+    for (let i = 0; i< str.length; ++i){
+      if (str[i-1] === ' '){
+        result +=str[i].toUpperCase()
       } else {
-        result+=str[i]
+        result += str[i]
       }
-     }
-     return result
+      return result;
+    }
+
+
     }
 
 
 module.exports = capitalize;
+
+
+// let result = str[0].toUpperCase()
+//      for (let i = 1; i<str.length; ++i){
+//       if (str[i-1] === " ") {
+//         result += str[i].toUpperCase()
+//       } else {
+//         result+=str[i]
+//       }
+//      }
+//      return result
