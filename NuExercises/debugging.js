2a2bbbc5538a2f85afde483227f4335512941a14
Ardita Burhop <kalendarperaden@gmail.com>
2021-01-21 23:07:53 -0600

    debugginSteps

---
+++ NuExercises/debugging.js
@@ -0,0 +1,12 @@
+/*
+-Add a 'debugger' statement in your function
+-Call the function manually
+-At the terminal run "node inspect index.js"
+-To continue execution of the file press c then enter ( c for continue)
+-To launch a repl(which provides val of arg or fux) type repl then enter
+-To exit the repl press control+c
+
+
+
+
+*/
