6ed4ee7f89a4d92bf24ee477c2e1900dd076954d
Ardita Burhop <kalendarperaden@gmail.com>
2021-02-08 00:23:20 -0600

    designQ

---
+++ NuExercises/buildingTwitter.js
@@ -0,0 +1,20 @@
+/*
+Interviewer is looking to see how you communicate what you are trying to say, and what areas are u focusing on when u answer this question.
+-Different interviewer will expect different asnwers.
+- Ask the question if there is a particular area they want you to focus on.
+- If no focus point is given, a good place to start is the data model or teh database of the given product. How you would build out the database of the product.
+Example: Think of how u would store tweets or users in teh database.
+
+--Pivot the discussion to the areas that you are MORE FAMILIAR WITH.
+-Dont focus on the specific frameworks u would use- this is the point of the this question.
+--If you can draw stuff out. It will help u and flow the conversation and communicate and help the interviewer to guide u.
+--Talk and communicate and voice what u are thinking.
+--As soon as they give u what to work on, ask a question
+--Pick 1-2 core features after teh drawing
+
+
+
+
+
+
+*/
--- NuExercises/debugging.js
+++ NuExercises/debugging.js
@@ -4,9 +4,7 @@
 -At the terminal run "node inspect index.js"
 -To continue execution of the file press c then enter ( c for continue)
 -To launch a repl(which provides val of arg or fux) type repl then enter
--To exit the repl press control+c
-
-
+-To exit the repl press control+
 
 
 */
